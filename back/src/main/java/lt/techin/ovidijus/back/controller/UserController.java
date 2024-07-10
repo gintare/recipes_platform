84baf89f619871b8e596b27021912fc64a858131
Ovidijus <ovidijus.e@gmail.com>
2024-07-10 14:51:38 +0300

    update register and login validation

--- back/src/main/java/lt/techin/ovidijus/back/controller/UserController.java
+++ back/src/main/java/lt/techin/ovidijus/back/controller/UserController.java
@@ -6,10 +6,9 @@ import lt.techin.ovidijus.back.dto.UserDTO;
 import lt.techin.ovidijus.back.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -34,5 +33,11 @@ public class UserController {
             return ResponseEntity.badRequest().body(loggedUser);
         }
         return ResponseEntity.ok(loggedUser);
+    }
+
+    @GetMapping("/emails")
+    public ResponseEntity<List<String>> getAllUserEmails() {
+        List<String> emails = userService.getAllUserEmails();
+        return ResponseEntity.ok(emails);
     }
 }
