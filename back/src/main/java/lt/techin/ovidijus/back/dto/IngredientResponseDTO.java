9e788741f88914121a701492900452c79cbefbf5
Valdastechinlt <valdemaras.gedziunas@stud.techin.lt>
2024-07-10 12:45:47 +0300

    More recipies CRUD

---
+++ back/src/main/java/lt/techin/ovidijus/back/dto/IngredientResponseDTO.java
@@ -0,0 +1,9 @@
+package lt.techin.ovidijus.back.dto;
+
+import lombok.Data;
+
+@Data
+public class IngredientResponseDTO {
+    private Long id;
+    private String title;
+}
--- back/src/main/java/lt/techin/ovidijus/back/service/RecipeService.java
+++ back/src/main/java/lt/techin/ovidijus/back/service/RecipeService.java
@@ -1,12 +1,24 @@
 package lt.techin.ovidijus.back.service;
 
+import lt.techin.ovidijus.back.dto.IngredientRequestDTO;
+import lt.techin.ovidijus.back.dto.IngredientResponseDTO;
 import lt.techin.ovidijus.back.dto.RecipeRequestDTO;
+import lt.techin.ovidijus.back.dto.RecipeResponseDTO;
+import lt.techin.ovidijus.back.exceptions.CategoryNotFoundException;
+import lt.techin.ovidijus.back.exceptions.RecipeNotFoundException;
+import lt.techin.ovidijus.back.exceptions.RequiredFieldIsEmptyException;
 import lt.techin.ovidijus.back.model.Category;
+import lt.techin.ovidijus.back.model.Ingredient;
 import lt.techin.ovidijus.back.model.Recipe;
 import lt.techin.ovidijus.back.repository.CategoryRepository;
 import lt.techin.ovidijus.back.repository.IngredientRepository;
 import lt.techin.ovidijus.back.repository.RecipeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class RecipeService {
@@ -21,15 +33,25 @@ public class RecipeService {
         this.ingredientRepository = ingredientRepository;
     }
 
-    public void createRecipe(RecipeRequestDTO recipeRequestDTO) {
+    public RecipeResponseDTO createRecipe(RecipeRequestDTO recipeRequestDTO) {
+        if(recipeRequestDTO.getName().isEmpty()){
+            throw new RequiredFieldIsEmptyException("Required name field is empty");
+        }
+        if(recipeRequestDTO.getDescription().isEmpty()){
+            throw new RequiredFieldIsEmptyException("Required description field is empty");
+        }
+        if(recipeRequestDTO.getInstructions().isEmpty()){
+            throw new RequiredFieldIsEmptyException("Required instructions field is empty");
+        }
+        if(recipeRequestDTO.getTimeInMinutes() == 0){
+            throw new RequiredFieldIsEmptyException("Required timeInMinutes field is 0");
+        }
         Category category = categoryRepository.findByName(recipeRequestDTO.getCategory().getName());
         if(category == null){
             category = new Category();
             category.setName(recipeRequestDTO.getCategory().getName());
             categoryRepository.save(category);
         }
-
-        
 
         Recipe recipe = new Recipe();
         recipe.setName(recipeRequestDTO.getName());
@@ -38,7 +60,65 @@ public class RecipeService {
         recipe.setInstructions(recipeRequestDTO.getInstructions());
         recipe.setTimeInMinutes(recipeRequestDTO.getTimeInMinutes());
         recipe.setCategory(category);
-
         this.recipeRepository.save(recipe);
+
+        Set<IngredientResponseDTO> ingredientResponseDTOSet = new LinkedHashSet<>();
+        for (IngredientRequestDTO ingredientDto : recipeRequestDTO.getIngredients()) {
+            Ingredient ingredient = new Ingredient();
+            ingredient.setTitle(ingredientDto.getTitle());
+            ingredient.addRecipe(recipe);
+            ingredientRepository.save(ingredient);
+
+            IngredientResponseDTO ingredientResponseDTO = new IngredientResponseDTO();
+            ingredientResponseDTO.setId(ingredient.getId());
+            ingredientResponseDTO.setTitle(ingredient.getTitle());
+            ingredientResponseDTOSet.add(ingredientResponseDTO);
+
+        }
+
+        RecipeResponseDTO recipeResponseDTO = new RecipeResponseDTO();
+        recipeResponseDTO.setId(recipe.getId());
+        recipeResponseDTO.setName(recipe.getName());
+        recipeResponseDTO.setDescription(recipe.getDescription());
+        recipeResponseDTO.setImage(recipe.getImage());
+        recipeResponseDTO.setInstructions(recipe.getInstructions());
+        recipeResponseDTO.setIngredients(ingredientResponseDTOSet);
+        return recipeResponseDTO;
+
+    }
+
+    public List<RecipeResponseDTO> findAll() {
+        List<Recipe> recipes = this.recipeRepository.findAll();
+        List<RecipeResponseDTO> recipeResponseDTOS = new ArrayList<>();
+        for(Recipe recipe : recipes){
+            RecipeResponseDTO recipeResponseDTO = new RecipeResponseDTO();
+            recipeResponseDTO.setId(recipe.getId());
+            recipeResponseDTO.setName(recipe.getName());
+            recipeResponseDTO.setDescription(recipe.getDescription());
+            recipeResponseDTO.setImage(recipe.getImage());
+            recipeResponseDTO.setInstructions(recipe.getInstructions());
+            recipeResponseDTOS.add(recipeResponseDTO);
+        }
+        return recipeResponseDTOS;
+    }
+
+    public RecipeResponseDTO updateRecipe(Long categoryId, Long recipeId, RecipeRequestDTO recipeRequestDTO) {
+        Category category = this.categoryRepository.findById(categoryId).orElseThrow(() -> new CategoryNotFoundException("No category found by id = "+categoryId));
+        Recipe recipe = this.recipeRepository.findById(recipeId).orElseThrow(() -> new RecipeNotFoundException("No recipe found with an id = "+recipeId));
+        recipe.setName(recipeRequestDTO.getName());
+        recipe.setImage(recipeRequestDTO.getImage());
+        recipe.setDescription(recipeRequestDTO.getDescription());
+        recipe.setInstructions(recipeRequestDTO.getInstructions());
+        recipe.setTimeInMinutes(recipeRequestDTO.getTimeInMinutes());
+        this.recipeRepository.save(recipe);
+
+        RecipeResponseDTO recipeResponseDTO = new RecipeResponseDTO();
+        recipeResponseDTO.setId(recipe.getId());
+        recipeResponseDTO.setName(recipe.getName());
+        recipeResponseDTO.setDescription(recipe.getDescription());
+        recipeResponseDTO.setImage(recipe.getImage());
+        recipeResponseDTO.setInstructions(recipe.getInstructions());
+
+        return recipeResponseDTO;
     }
 }
