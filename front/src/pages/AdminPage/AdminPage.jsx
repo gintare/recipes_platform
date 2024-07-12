07c2d0c27f97fb65ddc12a83e53123bffbb98e67
Ovidijus <ovidijus.e@gmail.com>
2024-07-12 09:30:22 +0300

    categories

--- front/src/pages/AdminPage/AdminPage.jsx
+++ front/src/pages/AdminPage/AdminPage.jsx
@@ -27,22 +27,12 @@ const AdminPage = () => {
   }, [update]);
 
   return (
-    // <div className='container admin-page d-flex flex-column align-items-center'>
-    //   <div className='row w-100'>
-    //     <div className='col-12 col-md-8 d-flex flex-column align-items-center'>
-    //       <h1 className='text-center mb-4'>Categories</h1>
-    //     </div>
-    //     <div className='col-12 col-md-4 d-flex justify-content-end align-items-end'>
-    //       <CategoriesForm />
-    //     </div>
-    //   </div>
-    // </div>
     <div className='row admin-page'>
       <h1 className='text-center mt-3'>Categories</h1>
       <div className='d-flex flex-column align-items-end mt-3'>
         <CategoriesForm className='col-12 col-md-8 d-flex flex-column' />
       </div>
-      <CategoriesList />
+      <CategoriesList className='d-grid col-12 col-md-8 col-xl-10' />
     </div>
   );
 };
