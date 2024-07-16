108436311ec6cd17a672178885966a50f749bbaa
Valdastechinlt <valdemaras.gedziunas@stud.techin.lt>
2024-07-16 09:03:24 +0300

    Frontent style improvements

--- front/src/pages/ProfilePage/ProfilePage.jsx
+++ front/src/pages/ProfilePage/ProfilePage.jsx
@@ -4,57 +4,90 @@ import { getRecipesByUserId } from "../../services/get";
 import ProfileRecipeCard from "../../Components/ProfileRecipeCard/ProfileRecipeCard";
 import RecipesForm from "../../Components/Forms/RecipesForm/RecipesForm";
 import RecipesContext from "../../Context/RecipesContentxt/RecipesContext";
+import "./ProfilePage.css";
 
-function ProfilePage(){
-    //const [recipies, setRecipies] = useState([]);
-    const [error, setError] = useState("");
-    //const [update, setUpdate] = useState(0);
-    const [createRecipeIsVisible, setCreateRecipeIsVisible] = useState(false);
-    const { id } = useContext(UserContext);
-    const {recipes, setRecipes, update, setUpdate, updateRecipeFormIsVisible, setUpdateRecipeFormIsVisible, updateRecipe, setUpdateRecipe} = useContext(RecipesContext);
-    console.log(`User id: ${id}`);
+function ProfilePage() {
+  //const [recipies, setRecipies] = useState([]);
+  const [error, setError] = useState("");
+  //const [update, setUpdate] = useState(0);
+  const [createRecipeIsVisible, setCreateRecipeIsVisible] = useState(false);
+  const { id } = useContext(UserContext);
+  const {
+    recipes,
+    setRecipes,
+    update,
+    setUpdate,
+    updateRecipeFormIsVisible,
+    setUpdateRecipeFormIsVisible,
+    updateRecipe,
+    setUpdateRecipe,
+  } = useContext(RecipesContext);
+  console.log(`User id: ${id}`);
 
-    useEffect(() => {
-        const getRecipes = async() => {
-          try{
-            const rec = await getRecipesByUserId(id);
-            setRecipes(rec);
-            console.log(rec);
-          }catch(error){
-            
-          }
-        }
-        getRecipes();
-    }, [update]);
+  useEffect(() => {
+    const getRecipes = async () => {
+      try {
+        const rec = await getRecipesByUserId(id);
+        setRecipes(rec);
+        console.log(rec);
+      } catch (error) {}
+    };
+    getRecipes();
+  }, [update]);
 
-
-    return(<><h1>Hello profile</h1>
-    <button type="button" className="btn btn-primary" onClick={() => {
-      setCreateRecipeIsVisible(!createRecipeIsVisible);
-      setUpdateRecipeFormIsVisible(false);
-      }}>Create New Recipe</button>
-      {/* <button type="button" className="btn btn-primary">Edit Recipe</button>
-      <button type="button" className="btn btn-primary">Delete Recipe</button>
-       */}
-       <button type="button" className="btn btn-primary" onClick={() => {
-      setCreateRecipeIsVisible(false);
-      setUpdateRecipeFormIsVisible(false);
-      setUpdate((prev) => prev+1);
-      }}>My recipes</button>
-
-    {createRecipeIsVisible && <RecipesForm setUpdate={setUpdate}/>}
-    {updateRecipeFormIsVisible && <RecipesForm setUpdate={setUpdate} recipe={updateRecipe}/>}
-
-    <div className="container text-center">
-      <div className="row row-cols-2 row-cols-lg-3 g-2 g-lg-3">
-        {recipes.map((recipe) => {
-          return <div key={recipe.id} className="col"> <ProfileRecipeCard recipe={recipe} createRecipeIsVisible={createRecipeIsVisible} setCreateRecipeIsVisible={setCreateRecipeIsVisible} /> </div>
-        })}
+  return (
+    <>
+      <div className="buttons-content">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => {
+            setCreateRecipeIsVisible(!createRecipeIsVisible);
+            setUpdateRecipeFormIsVisible(false);
+          }}
+        >
+          Create New Recipe
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => {
+            setCreateRecipeIsVisible(false);
+            setUpdateRecipeFormIsVisible(false);
+            setUpdate((prev) => prev + 1);
+          }}
+        >
+          My recipes
+        </button>
       </div>
-    </div>
-    
-    
-    </>);
+
+      {createRecipeIsVisible && <RecipesForm setUpdate={setUpdate} />}
+      {updateRecipeFormIsVisible && (
+        <RecipesForm setUpdate={setUpdate} recipe={updateRecipe} />
+      )}
+
+      <div className="container text-center">
+        <div className="row row-cols-2 row-cols-lg-3 g-2 g-lg-3">
+          {recipes.map((recipe) => {
+            return (
+              <div key={recipe.id} className="col">
+
+                <ProfileRecipeCard
+                  recipe={recipe}
+                  createRecipeIsVisible={createRecipeIsVisible}
+                  setCreateRecipeIsVisible={setCreateRecipeIsVisible}
+                />
+              </div>
+            );
+          })}
+        </div>
+        
+      </div>
+      <div className="footer-padding">
+
+      </div>
+    </>
+  );
 }
 
 export default ProfilePage;
