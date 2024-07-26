8d9a89351528c648021502d716a978f19b35a380
Ovidijus <ovidijus.e@gmail.com>
2024-07-26 12:19:20 +0300

    add go back button

--- front/src/Components/RecipeCard/RecipeCard.jsx
+++ front/src/Components/RecipeCard/RecipeCard.jsx
@@ -1,4 +1,3 @@
-
 import Card from 'react-bootstrap/Card';
 import LikeButton from '../LikeButton/LikeButton';
 import './RecipeCard.css';
@@ -6,21 +5,18 @@ import { Link } from 'react-router-dom';
 import UserContext from '../../Context/UserContext/UserContext';
 import { useContext } from 'react';
 
-
 const RecipeCard = ({ recipe }) => {
-  const{id}=useContext(UserContext);
+  const { id } = useContext(UserContext);
 
   return (
-    <Card className="recipe-card">
-     <Link to={`/recipe/${recipe.id}`} key={recipe.id}>
-      <Card.Img variant="top" src={recipe.image} alt={recipe.title} />
+    <Card className='recipe-card'>
+      <Link to={`/recipe/${recipe.id}`} key={recipe.id}>
+        <Card.Img variant='top' src={recipe.image} alt={recipe.title} />
       </Link>
       <Card.Body>
-        <Card.Title className="recipe-card-title">{recipe.name}</Card.Title>
-        <Card.Text>
-          Preparation time: {recipe.timeInMinutes} min
-        </Card.Text>
-        <LikeButton recipeId={recipe.id} userId={id}/>
+        <Card.Title className='recipe-card-title'>{recipe.name}</Card.Title>
+        <Card.Text>Preparation time: {recipe.timeInMinutes} min</Card.Text>
+        <LikeButton recipeId={recipe.id} userId={id} />
       </Card.Body>
     </Card>
   );
--- front/src/pages/RecipeDetailsPage/RecipeDetailsPage.jsx
+++ front/src/pages/RecipeDetailsPage/RecipeDetailsPage.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useEffect, useContext } from 'react';
+import { Link, useParams, useLocation, useNavigate } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import {
   getCommentsByRecipe,
   getIsFavorite,
@@ -7,31 +8,29 @@ import {
   getOneCategory,
   getOneRecipe,
   getOneUser,
-} from "../../services/get";
-import "./RecipeDetailsPage.css";
-import UserContext from "../../Context/UserContext/UserContext";
-import { commentPost, favoritePost, followerPost } from "../../services/post";
-import {
-  deleteComment,
-  deleteFavorite,
-  deleteFollower,
-} from "../../services/delete";
-import { BarChartLineFill, HeartFill, Heart } from "react-bootstrap-icons";
-import { useForm } from "react-hook-form";
-import NotFoundPage from "../NotFoundPage/NotFoundPage";
-import { OverlayTrigger, Popover } from "react-bootstrap";
+} from '../../services/get';
+import './RecipeDetailsPage.css';
+import UserContext from '../../Context/UserContext/UserContext';
+import { commentPost, favoritePost, followerPost } from '../../services/post';
+import { deleteComment, deleteFavorite, deleteFollower } from '../../services/delete';
+import { BarChartLineFill, HeartFill, Heart } from 'react-bootstrap-icons';
+import { useForm } from 'react-hook-form';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import { OverlayTrigger, Popover } from 'react-bootstrap';
 
 function RecipeDetailsPage() {
   const { id: recipeId } = useParams();
   const [recipe, setRecipe] = useState({});
   const [category, setCategory] = useState({});
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
   const [favorite, setFavorite] = useState(false);
   const [comments, setComments] = useState([]);
-  const [recipeCreatorUserName, setRecipeCreatorUserName] = useState("");
+  const [recipeCreatorUserName, setRecipeCreatorUserName] = useState('');
   const [update, setUpdate] = useState(0);
   const { id, userName, isLoggedIn } = useContext(UserContext);
   const [follow, setFollow] = useState(true);
+  const location = useLocation();
+  // const navigate = useNavigate();
   const {
     register,
     control,
@@ -42,17 +41,12 @@ function RecipeDetailsPage() {
     watch,
   } = useForm({
     defaultValues: {
-      text: "",
+      text: '',
     },
   });
 
-  // console.log("user id = "+id);
-  //console.log("recipe Id = " + recipeId);
-
   const followUser = async () => {
-    //console.log("follow me");
     setFollow((follow) => !follow);
-    //console.log("follow me follow = " + follow);
     if (!follow == true) {
       const fol = await followerPost(id, recipe.userId);
     } else {
@@ -61,7 +55,6 @@ function RecipeDetailsPage() {
   };
 
   const clickFavoriteHandler = () => {
-    //console.log("favorite me");
     setFavorite((favorite) => !favorite);
 
     if (!favorite) {
@@ -72,22 +65,18 @@ function RecipeDetailsPage() {
   };
 
   const onCommentsSubmitClickHandler = (data) => {
-    //console.log(recipe.id)
-    //console.log(data);
     const postData = commentPost(id, recipe.id, data);
-    //console.log(postData);
     reset();
     setUpdate((prev) => prev + 1);
   };
 
   const deleteCommentClickHandler = (commentId) => {
-    //console.log("commentId = "+commentId);
     deleteComment(commentId);
     setUpdate((prev) => prev + 1);
   };
 
   const popoverHoverFocus = (
-    <Popover id="popover-trigger-hover-focus" title="Popover bottom">
+    <Popover id='popover-trigger-hover-focus' title='Popover bottom'>
       Add to <strong>favorites</strong>
     </Popover>
   );
@@ -96,22 +85,16 @@ function RecipeDetailsPage() {
     const getRecipe = async () => {
       try {
         const rec = await getOneRecipe(recipeId);
-        // console.log(rec);
-        //console.log(rec.ingredients);
         setRecipe(rec);
         const cat = await getOneCategory(rec.categoryId);
-        //console.log(cat);
         setCategory(cat);
         const userCreator = await getOneUser(rec.userId);
-        //console.log(userCreator);
         setRecipeCreatorUserName(userCreator.userName);
 
-        if(id){
+        if (id) {
           const is = await getIsFollower(id, rec.userId);
-          //console.log(is);
           setFollow(is);
           const isFav = await getIsFavorite(id, rec.id);
-          //console.log("isFav = "+isFav);
           setFavorite(isFav);
           const comm = await getCommentsByRecipe(rec.id);
           setComments(comm);
@@ -123,64 +106,69 @@ function RecipeDetailsPage() {
     getRecipe();
   }, [update]);
 
+  // let history = useHistory();
+  const navigate = useNavigate();
+
   return (
     <>
-      {/* <h1>Hello Recipe = {recipeId}</h1> */}
-      <div className="container-lg">container</div>
-      <div className="container">
-      {isLoggedIn && 
-        <div className="row">
-          <div className="col-sm-2">Like Likes button</div>
-          <div className="col-sm-2">
-            {favorite ? (
-              
-              <HeartFill color="red" size="36" onClick={clickFavoriteHandler} />
-            ) : (
-              <OverlayTrigger
-              trigger={['hover', 'focus']}
-              placement="top"
-              overlay={popoverHoverFocus}
-            >
-              <Heart color="red" size="36" onClick={clickFavoriteHandler} />
-              </OverlayTrigger>
-            )}
-          </div>
-          <div className="col">
-            <div className="author-content">Author : {recipeCreatorUserName}</div>
-            <div className="author-button-content">
-            <button
-              className={follow ? "follow_button_active" : "follow_button"}
-              onClick={followUser}
-            >
-              {follow ? "You are following author" : "Follow author"}
-            </button>
+      <button className='recipes-link' onClick={() => navigate(-1)}>
+        <i className='bi bi-arrow-left-circle back-button'>
+          <span className='back-text'>Go back</span>
+        </i>
+      </button>
+      <div className='container'>
+        {isLoggedIn && (
+          <div className='row'>
+            <div className='col-sm-2'>Like Likes button</div>
+            <div className='col-sm-2'>
+              {favorite ? (
+                <HeartFill color='red' size='36' onClick={clickFavoriteHandler} />
+              ) : (
+                <OverlayTrigger
+                  trigger={['hover', 'focus']}
+                  placement='top'
+                  overlay={popoverHoverFocus}
+                >
+                  <Heart color='red' size='36' onClick={clickFavoriteHandler} />
+                </OverlayTrigger>
+              )}
+            </div>
+            <div className='col'>
+              <div className='author-content'>Author : {recipeCreatorUserName}</div>
+              <div className='author-button-content'>
+                <button
+                  className={follow ? 'follow_button_active' : 'follow_button'}
+                  onClick={followUser}
+                >
+                  {follow ? 'You are following author' : 'Follow author'}
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-         }
-        <div className="row">
-          <div className="col-md-2 image-content">
-            <img src={recipe.image} alt="recipe_photo" />
+        )}
+        <div className='row'>
+          <div className='col-md-2 image-content'>
+            <img src={recipe.image} alt='recipe_photo' />
           </div>
-          <div className="col recipe-info-content">
-            <h5 className="card-title">{recipe.name}</h5>
+          <div className='col recipe-info-content'>
+            <h5 className='card-title'>{recipe.name}</h5>
             <br />
-            <label htmlFor="description" className="col col-form-label">
+            <label htmlFor='description' className='col col-form-label'>
               Description:
             </label>
-            <div className="col ">{recipe.description}</div>
-            <label htmlFor="instructions" className="col col-form-label">
+            <div className='col '>{recipe.description}</div>
+            <label htmlFor='instructions' className='col col-form-label'>
               Instructions:
             </label>
-            <div className="col ">{recipe.instructions}</div>
-            <label htmlFor="categoryName" className="col col-form-label">
+            <div className='col '>{recipe.instructions}</div>
+            <label htmlFor='categoryName' className='col col-form-label'>
               Recipe category:
             </label>
-            <div className="col ">{category.name}</div>
-            <label htmlFor="ingredients" className="col col-form-label">
+            <div className='col '>{category.name}</div>
+            <label htmlFor='ingredients' className='col col-form-label'>
               Ingredients:
             </label>
-            <ul id="ingredients">
+            <ul id='ingredients'>
               {recipe.ingredients &&
                 recipe.ingredients.map((ingredient, index) => (
                   <li key={index} value={ingredient.id}>
@@ -191,59 +179,56 @@ function RecipeDetailsPage() {
             Preparation time : {recipe.timeInMinutes}
           </div>
         </div>
-        {isLoggedIn && 
-        <>
-        <div className="row">
-          <form
-            noValidate
-            onSubmit={handleSubmit(onCommentsSubmitClickHandler)}
-          >
-            <div className="mb-3">
-              <label htmlFor="commentsTextarea" className="form-label">
-                Comment text
-              </label>
-              <textarea
-                className="form-control"
-                id="commentsTextarea"
-                rows="3"
-                {...register("text", {
-                  required: "Comment text is required",
-                })}
-              ></textarea>
-            </div>
-            <div className="col-auto">
-              <button type="submit" className="btn btn-primary mb-3">
-                Comment
-              </button>
-            </div>
-          </form>
-        </div>
-        {comments.length > 0 ? "Comments:" : ""}
-        {comments.map((comment) => {
-          return (
-            <div key={comment.id} className="row">
-              <div key={comment.id} className="mb-3">
-                &#62; {comment.text}{" "}
-                {id == comment.userId && (
-                  <button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      deleteCommentClickHandler(comment.id);
-                    }}
-                    className="btn btn-primary mb-3"
-                  >
-                    Delete 🗑️
+        {isLoggedIn && (
+          <>
+            <div className='row'>
+              <form noValidate onSubmit={handleSubmit(onCommentsSubmitClickHandler)}>
+                <div className='mb-3'>
+                  <label htmlFor='commentsTextarea' className='form-label'>
+                    Comment text
+                  </label>
+                  <textarea
+                    className='form-control'
+                    id='commentsTextarea'
+                    rows='3'
+                    {...register('text', {
+                      required: 'Comment text is required',
+                    })}
+                  ></textarea>
+                </div>
+                <div className='col-auto'>
+                  <button type='submit' className='btn btn-primary mb-3'>
+                    Comment
                   </button>
-                )}
-              </div>
-              <div></div>
+                </div>
+              </form>
             </div>
-          );
-        })}
-        </>
-        }
-        
-        <div className="footer_padding"></div>
+            {comments.length > 0 ? 'Comments:' : ''}
+            {comments.map((comment) => {
+              return (
+                <div key={comment.id} className='row'>
+                  <div key={comment.id} className='mb-3'>
+                    &#62; {comment.text}{' '}
+                    {id == comment.userId && (
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          deleteCommentClickHandler(comment.id);
+                        }}
+                        className='btn btn-primary mb-3'
+                      >
+                        Delete 🗑️
+                      </button>
+                    )}
+                  </div>
+                  <div></div>
+                </div>
+              );
+            })}
+          </>
+        )}
+
+        <div className='footer_padding'></div>
       </div>
     </>
   );
