1b358a76cb3e1f9511d3c9ce67158e0770746150
Slavomir Michailin <slavikbanana87@gmail.com>
2024-07-26 12:05:25 +0300

    Fix small isues 4

--- back/src/main/java/lt/techin/ovidijus/back/repository/RecipeRepository.java
+++ back/src/main/java/lt/techin/ovidijus/back/repository/RecipeRepository.java
@@ -6,6 +6,7 @@ import lt.techin.ovidijus.back.model.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
@@ -18,6 +19,8 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long>  {
 
     List<Recipe> findByCategory(Category category, Pageable page);
 
+    @Query("SELECT r FROM Recipe r WHERE r.name LIKE %:searchTerm%")
+    List<Recipe> findByNameContaining(@Param("searchTerm") String searchTerm, Pageable page);
 //    @Query("SELECT u FROM Recipe u WHERE u.user.id = 1")
 //    Collection<Recipe> findByUser();
 }
